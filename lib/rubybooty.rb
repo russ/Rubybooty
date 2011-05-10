fc7c47331c027efc19aeae7635191031c538b792
Russ Smith <russ@bashme.org>
2011-05-10 10:02:52 -0700

    Some better notes.

--- lib/rubybooty.rb
+++ lib/rubybooty.rb
@@ -12,13 +12,13 @@ module Rubybooty
     entrants = {}
 
     while true
-      name = ask("Entrants Name: ")
+      name = ask("Entrants Name (enter 'done' to finish): ")
       break if name.downcase == "done"
-      entries = ask("Number of Entries: ", Integer)
+      entries = ask("Number of Entries: (integer only)", Integer)
       entrants[name] = entries
     end
 
-    agree("Run Now? ")
+    agree("Run Now? (yes/no)")
 
     entries = []
     entrants.each { |k,v| v.times { entries << k }}
